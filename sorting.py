1c87d5f6d8557a2d61ef9e516ff21a203b7177e3
us579 <wzus579@gmail.com>
2019-04-09 17:15:56 +1000

    commit

--- sorting.py
+++ sorting.py
@@ -1,24 +1,29 @@
 #sorting algorithem
 
+
+#################################################
 #bubble sorting
 
-L = [3,5,15,2,32,12,43,23,1,5,6,3,435,43,21]
+L = [7,8,5,2,4,6,3]
 length = len(L)
 #time complexity O(n^2)
-def bubble_sorting(L,length):
+def bubble_sort(L,length):
     if length < 2:
         return L
     for i in range(length-1):
+        print(L)
         for j in range(length-i-1):
             if L[j+1] < L[j]:
                 L[j+1] ,L[j] = L[j],L[j+1]
     return L
 
-#print(bubble_sorting(L,length))
-        
-    
-    
-def Quick_sorting1(L):
+#print(bubble_sort(L,length))
+
+#################################################
+
+#quick sorting
+#time complexity O(nlog(n))
+def Quick_sort1(L):
     if len(L) <= 1:
         return L
     l = []
@@ -27,12 +32,12 @@ def Quick_sorting1(L):
     for i in L:
         if i < key :
              l.append(i)
-        
         else:
             r.append(i)
+            
     return quick_sorting(l) + [key] + quick_sorting(r)
 
-def Quick_sorting2(L,l,r):
+def Quick_sort2(L,l,r):
     if l>r:
         return L
     key = L[l]
@@ -46,12 +51,62 @@ def Quick_sorting2(L,l,r):
             l += 1
         L[r] = L[l]
     L[l] = key
-    Quick_sorting2(L,low,l-1)
-    Quick_sorting2(L,r+1,high)
+    Quick_sort2(L,low,l-1)
+    Quick_sort2(L,r+1,high)
     return L[::-1]
         
     
-print(Quick_sorting2(L,0,len(L)-1))
+#print(Quick_sorting2(L,0,len(L)-1))
+
+
+#################################################
+
+#selection sorting
+#time complexity O(n^2)
+def Selection_sort1(L):
+    lis = []
+    for i in range(len(L)):
+        small = min(L)
+        lis.append(L.pop(L.index(small)))
+    return lis
+
+def Selection_sort2(L):
+    length = len(L)
+    for index in range(length):
+        for i in range(index,length):
+            if L[index] > L[i]:
+                L[i],L[index] = L[index],L[i]
+    return L
+                
+#print(Selection_sort2(L))       
+
+#################################################
+
+#insert sort
+#time complexity :
+#平均和最坏情况下的时间复杂度都是O(n^2)，最好情况下都是O(n)
+def insert_sort(L):
+    length = len(L)
+    for i in range(length):
+        for j in range(i-1,-1,-1):
+            if L[i] > L[j]:
+                continue
+            else:
+                L[i] ,L[j] = L[j] , L[i]
+                i -=1
+    print L
+
+#insert_sort(L)
+
+
+
+
+
+
+
+
+
+        
 
     
     
