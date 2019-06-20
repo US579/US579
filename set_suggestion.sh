548a40e22c2df9b611c1b9c35c64e45455ba6ba6
US579 <wzus579@Gmail.com>
2019-06-21 01:37:52 +1000

    add suggestion zsh script

---
+++ set_suggestion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+cat "antigen bundle zsh-users/zsh-autosuggestions" >> ~/.zshrc
+
+
+
