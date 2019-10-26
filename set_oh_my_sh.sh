d173d0968e63b2728a1d7b8ab3d76b211787f599
EC2 Default User <ec2-user@ip-172-31-36-210.us-east-2.compute.internal>
2019-10-26 20:59:30 +0000

    modify

--- set_oh_my_sh.sh
+++ set_oh_my_sh.sh
@@ -1,6 +1,5 @@
-!/bin/bash
-apt-get update
-apt-get install zsh -y
+!#/bin/bash
+sudo yum install zsh -y
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
  
  
